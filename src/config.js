1add6eab61e85ecf9768380b7364e3dbd73c7cc2
Luis Jaimes <ferbluis143@gmail.com>
2023-12-01 22:05:20 -0300

    confg connection to mongocb.net

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 // Definición del puerto en el que se ejecutará el servidor
 export const PORT = 8080;
 
-//export const MONGODB_CNX_STR = "mongodb+srv://elejaimes:<password>@coderhouse.dwc12j1.mongodb.net/";
-export const MONGODB_CNX_STR = "mongodb://127.0.0.1:27017/coderhouse";
+export const MONGODB_CNX_STR =
+  "mongodb+srv://elejaimes:gatitos26@coderhouse.dwc12j1.mongodb.net/coderhouse"; // Conexión mongodb red
+//export const MONGODB_CNX_STR = "mongodb://127.0.0.1:27017/coderhouse"; // conexión mongodb local
